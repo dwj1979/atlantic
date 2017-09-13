0386c626cb23f0ac4a7fa0810ee59528af52c35b
zhuhongyu <2352539448@qq.com>
2017-09-13 13:28:58 +0800

    加建表语句跟页面。去掉不用的东西

--- src/main/java/com/zhu/controller/UserController.java
+++ src/main/java/com/zhu/controller/UserController.java
@@ -1,25 +1,26 @@
 package com.zhu.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.zhu.dao.UserMapper;
 import com.zhu.po.User;
-import com.zhu.po.UserExample;
-import com.zhu.tool.GeneralKeyTool;
 
 @RestController
 public class UserController {
 	
 	@Resource
 	UserMapper userMapper;
+	
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
     @RequestMapping("/adduser/{username}/{psd}")
     public String add(@PathVariable String username) {
     		return null;
---
+++ src/main/resources/database/atlantic1.sql
@@ -0,0 +1,140 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50719
+ Source Host           : localhost
+ Source Database       : atlantic1
+
+ Target Server Type    : MySQL
+ Target Server Version : 50719
+ File Encoding         : utf-8
+
+ Date: 09/13/2017 10:49:30 AM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(30) DEFAULT NULL,
+  `user_name` varchar(10) NOT NULL,
+  `user_psd` varchar(30) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_1`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_1`;
+CREATE TABLE `user_1` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(30) DEFAULT NULL,
+  `user_name` varchar(10) NOT NULL,
+  `user_psd` varchar(30) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_2`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_2`;
+CREATE TABLE `user_2` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(30) DEFAULT NULL,
+  `user_name` varchar(10) NOT NULL,
+  `user_psd` varchar(30) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_3`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_3`;
+CREATE TABLE `user_3` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(30) DEFAULT NULL,
+  `user_name` varchar(10) NOT NULL,
+  `user_psd` varchar(30) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `addr` int(11) DEFAULT NULL,
+  `gender` tinyint(4) NOT NULL DEFAULT '0',
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_info_1`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info_1`;
+CREATE TABLE `user_info_1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `addr` int(11) DEFAULT NULL,
+  `gender` tinyint(4) NOT NULL DEFAULT '0',
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_info_2`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info_2`;
+CREATE TABLE `user_info_2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `addr` int(11) DEFAULT NULL,
+  `gender` tinyint(4) NOT NULL DEFAULT '0',
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_info_3`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info_3`;
+CREATE TABLE `user_info_3` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `addr` int(11) DEFAULT NULL,
+  `gender` tinyint(4) NOT NULL DEFAULT '0',
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `utime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
