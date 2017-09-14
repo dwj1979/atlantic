8df913ed73ead63fd55176c4866bc98a7f3e65da
zhuhongyu <2352539448@qq.com>
2017-09-14 19:53:54 +0800

    页面通

--- src/main/java/com/zhu/Application.java
+++ src/main/java/com/zhu/Application.java
@@ -11,6 +11,5 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("**********************start finish**********************");
 	}
 }
--- src/main/java/com/zhu/controller/UserController.java
+++ src/main/java/com/zhu/controller/UserController.java
@@ -5,28 +5,23 @@ import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.zhu.dao.UserMapper;
 import com.zhu.po.User;
 
 @RestController
 public class UserController {
-	
+
 	@Resource
 	UserMapper userMapper;
-	
-	
-	@RequestMapping("/")
-	public ModelAndView index() {
-		return new ModelAndView("index");
+
+	@RequestMapping("/adduser/{username}/{psd}")
+	public String add(@PathVariable String username) {
+		return null;
 	}
-    @RequestMapping("/adduser/{username}/{psd}")
-    public String add(@PathVariable String username) {
-    		return null;
-    }
-    @RequestMapping("/finduser/{username}")
-    public User find(@PathVariable String username) {
-    		return null;
-    }
+
+	@RequestMapping("/finduser/{username}")
+	public User find(@PathVariable String username) {
+		return null;
+	}
 }
